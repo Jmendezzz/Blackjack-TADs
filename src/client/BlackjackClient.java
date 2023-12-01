c9236d278488cc76c802c713269f2c5e1ef2d2a4
Y00w1 <dgomez1019@cue.edu.co>
2023-12-01 17:33:54 -0500

    add the datastructures, card and client for test

--- src/client/BlackjackClient.java
+++ src/client/BlackjackClient.java
@@ -1,4 +1,42 @@
 package client;
 
+import java.io.*;
+import java.net.Socket;
+import java.net.URI;
+
 public class BlackjackClient {
+    private Socket socket;
+    private BufferedReader in;
+    private BufferedWriter out;
+
+    public BlackjackClient(String serverUrl) {
+        try {
+            URI uri = new URI(serverUrl);
+            String host = uri.getHost();
+            int port = uri.getPort() != -1 ? uri.getPort() : 80; // Use default port 80 if not specified
+            socket = new Socket(host, port);
+            in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            out = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+        } catch (Exception e) {
+            System.out.println("Error connecting to server: " + e);
+        }
+    }
+    public void sendMessage(String message) {
+        try {
+            out.write(message + "\n");
+            out.flush();
+        } catch (IOException e) {
+            System.out.println("Error sending message: " + e);
+        }
+    }
+    public void close() {
+        try {
+            in.close();
+            out.close();
+            socket.close();
+        } catch (IOException e) {
+            System.out.println("Error closing resources: " + e);
+        }
+    }
+
 }
--- src/model/Card.java
+++ src/model/Card.java
@@ -1,4 +1,8 @@
 package model;
 
 public class Card {
+    private String name;
+    private String suit;
+    private int pointValue;
+    private boolean isFaceUp;
 }
