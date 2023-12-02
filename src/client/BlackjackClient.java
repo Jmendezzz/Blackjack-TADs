df31bcd938b63b9df7a0e10bce2690bda55086a9
Y00w1 <dgomez1019@cue.edu.co>
2023-12-02 16:50:51 -0500

    client 1

--- src/client/BlackjackClient.java
+++ src/client/BlackjackClient.java
@@ -1,33 +1,55 @@
 package client;
 
+import model.Player;
+
 import java.io.*;
 import java.net.Socket;
 import java.net.URI;
+import java.util.Scanner;
 
 public class BlackjackClient {
     private Socket socket;
     private BufferedReader in;
     private BufferedWriter out;
+    private Player player;
+    Scanner scanner = new Scanner(System.in);
 
-    public BlackjackClient(String serverUrl) {
+    public BlackjackClient(String serverAddress, int serverPort) throws IOException {
+        this.connect(serverAddress, serverPort);
+        this.setPlayer();
+        Thread serverListenerThread = new Thread(this::listenToServer);
+        serverListenerThread.start();
+    }
+    private void connect(String serverAddress, int serverPort){
         try {
-            URI uri = new URI(serverUrl);
-            String host = uri.getHost();
-            int port = uri.getPort() != -1 ? uri.getPort() : 80; // Use default port 80 if not specified
-            socket = new Socket(host, port);
+            socket = new Socket(serverAddress, serverPort);
             in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
             out = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
         } catch (Exception e) {
             System.out.println("Error connecting to server: " + e);
         }
     }
-    public void sendMessage(String message) {
+    private void setPlayer() throws IOException {
+        System.out.println("Enter your name: ");
+        String name = scanner.next();
+        this.player = new Player(name);
+        this.player.setCash(500.0);
+        System.out.println("Welcome " + this.player.getName() + "!");
+    }
+    private void listenToServer() {
         try {
-            out.write(message + "\n");
-            out.flush();
+            while (true) {
+                String messageFromServer = in.readLine();
+                if (messageFromServer != null) {
+                    handleServerMessage(messageFromServer);
+                }
+            }
         } catch (IOException e) {
-            System.out.println("Error sending message: " + e);
+            System.out.println("Error reading from server: " + e);
         }
+    }
+    private void handleServerMessage(String message) {
+        System.out.println("Received from server: " + message);
     }
     public void close() {
         try {
@@ -38,5 +60,4 @@ public class BlackjackClient {
             System.out.println("Error closing resources: " + e);
         }
     }
-
 }
--- src/model/Player.java
+++ src/model/Player.java
@@ -1,9 +1,25 @@
 package model;
 
+import datastructures.Stack;
+
 public class Player {
   private String name;
+  private Double cash;
+  private Stack hand;
   public Player(String name) {
     this.name = name;
+  }
+  public Double getCash() {
+    return cash;
+  }
+  public void setCash(Double cash) {
+    this.cash = cash;
+  }
+  public Stack getHand() {
+    return hand;
+  }
+  public void setHand(Stack hand) {
+    this.hand = hand;
   }
   public String getName() {
     return name;
