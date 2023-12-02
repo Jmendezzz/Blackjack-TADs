759e5c6210106f0f203b6095628233a521d6a142
Juan Gerardo <jmendez1025@cue.edu.co>
2023-12-02 13:51:18 -0500

    feat: server test

--- src/server/BlackjackServer.java
+++ src/server/BlackjackServer.java
@@ -1,4 +1,32 @@
 package server;
 
+import java.io.DataInputStream;
+import java.io.EOFException;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+
 public class BlackjackServer {
+
+  public static void main(String[] args) {
+    try {
+      ServerSocket serverSocket = new ServerSocket(8080,10);
+      while(!serverSocket.isClosed()){
+        try {
+          System.out.println("Waiting for a client to connect...");
+          Socket socket = serverSocket.accept();
+          System.out.println("Client connected!");
+          DataInputStream dataInputStream = new DataInputStream(socket.getInputStream());
+          String message = dataInputStream.readUTF();
+          System.out.println("Message from client: " + message);
+        }catch (EOFException e){
+          System.out.println("Cloudflare!");
+        }
+
+      }
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    }
+  }
 }
